e4e91a4618c823267d8bf1b54783a1a37cbe8325
Thiago <thiago.magsoares@gmail.com>
2013-05-25 18:35:41 -0300

    Noticia destaque, widget most popular

--- wp-content/themes/dbandeja/index.php
+++ wp-content/themes/dbandeja/index.php
@@ -4,7 +4,38 @@
 	<div class="content">
 		<article class="article">
 			<div id="content_box">
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php $p = 1; if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					<?php if($p == 1 && is_home()){ ?>
+
+					<div class="post highlight <?php echo (++$j % 2 == 0) ? 'last' : ''; ?>">
+						<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="nofollow" id="featured-thumbnail">
+							<?php if ( has_post_thumbnail() ) { ?> 
+								<?php echo '<div class="featured-thumbnail">'; the_post_thumbnail('featured',array('title' => '')); echo '</div>'; ?>
+							<?php } else { ?>
+								<div class="featured-thumbnail">
+									<img width="100%" src="<?php echo get_template_directory_uri(); ?>/images/nothumb.png" class="attachment-featured wp-post-image" alt="<?php the_title(); ?>">
+								</div>
+							<?php } ?>
+						</a>
+                        <header>						
+							<div class="title">
+								<div class="thetime">
+									<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php _e(' ','mythemeshop'); the_time('j/n/Y'); ?></a>
+								</div>
+								<span class="thecomment"><a href="<?php comments_link(); ?>"><?php comments_number('0','1','%'); ?></a> Comentários</span>
+							</div>
+						</header><!--.header-->
+						<div class="post-content image-caption-format-1">
+							<h2 class="home-title"><?php the_title(); ?></h2>
+							<div class="excerpt"><?php echo excerpt(17);?></div>
+							<a class="read-more" href="">Ver Mais</a>
+						</div>
+                        <div style="display:none" class="readMore"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php _e('Read More','mythemeshop'); ?></a></div>
+					</div><!--.post excerpt-->
+
+					<?php }else{ ?>
+
 					<div class="post excerpt <?php echo (++$j % 2 == 0) ? 'last' : ''; ?>">
 						<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="nofollow" id="featured-thumbnail">
 							<?php if ( has_post_thumbnail() ) { ?> 
@@ -30,7 +61,10 @@
 						</div>
                         <div style="display:none" class="readMore"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php _e('Read More','mythemeshop'); ?></a></div>
 					</div><!--.post excerpt-->
-				<?php endwhile; else: ?>
+
+					<?php } ?>
+
+				<?php $p++; endwhile; else: ?>
 					<div class="post excerpt">
 						<div class="no-results">
 							<p><strong><?php _e('There has been an error.', 'mythemeshop'); ?></strong></p>
